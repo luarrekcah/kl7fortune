cd550b70a096976c33911447fb4715382685c59a
luarrekcah <raulrodriguesdemoraes@gmail.com>
2023-08-27 18:13:32 -0500

    a

--- js/app.js
+++ js/app.js
@@ -25,23 +25,3 @@ function countdown(t) {
 function updateTime() {
   return dayjs().add(2, "m").format("HH:mm");
 }
-button.addEventListener("click", (t) => {
-  Swal.fire({
-    title: "Atenção!",
-    text: "O sinal só funcionará na plataforma abaixo \n ↓",
-    icon: "info",
-    confirmButtonText: "Entendi",
-    customClass: {
-      popup: "my-popup-class",
-      // adicionando a classe personalizada ao popup
-      title: "my-title-class",
-      content: "my-content-class",
-    },
-  });
-  t.preventDefault(),
-    (button.disabled = !0),
-    (output.innerText = getRandomNumber(5, 10)),
-    (output2.innerText = getRandomNumber(5, 10)),
-    (output3.innerText = updateTime()),
-    countdown(60);
-});
